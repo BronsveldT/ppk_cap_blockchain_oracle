7f058a3278d65951bfc0b9ce7fedf38fd597d504
Thomas Bronsveld <Thomasbronsveld94@gmail.com>
2024-04-28 16:20:48 +0200

    start 2024 blockchain oracle

---
+++ ppk_blockchain/src/main/java/com/capgemini/ppk_blockchain/PpkBlockchainApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.ppk_blockchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpkBlockchainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpkBlockchainApplication.class, args);
+	}
+
+}
---
+++ ppk_blockchain/src/test/java/com/capgemini/ppk_blockchain/PpkBlockchainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.ppk_blockchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpkBlockchainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
