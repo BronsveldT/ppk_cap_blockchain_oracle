e7de46d03a051ce62d67fe4ab50bc92f2f1565e8
Thomas Bronsveld <Thomasbronsveld94@gmail.com>
2024-06-27 13:45:22 +0200

    Seperated the singular controller into multiple controllers that handle requests for specific data objects

---
+++ ppk_blockchain/src/main/java/com/capgemini/ppk_blockchain/web/controllers/BaseController.java
@@ -0,0 +1,8 @@
+package com.capgemini.ppk_blockchain.web.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/v1")
+public class BaseController {
+}
