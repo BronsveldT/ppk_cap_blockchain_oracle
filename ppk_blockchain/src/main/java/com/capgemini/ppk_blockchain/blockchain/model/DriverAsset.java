74ca43a7411522fe46b4993d99ca0a535a2557e4
Thomas Bronsveld <Thomasbronsveld94@gmail.com>
2024-06-27 13:22:57 +0200

    Moved the calculation of the travelling costs to a util method, still need to move addTravelledInformation

--- ppk_blockchain/src/main/java/com/capgemini/ppk_blockchain/blockchain/model/DriverAsset.java
+++ ppk_blockchain/src/main/java/com/capgemini/ppk_blockchain/blockchain/model/DriverAsset.java
@@ -12,12 +12,6 @@ import java.util.HashMap;
 @ToString
 public class DriverAsset {
 
-
-    @ToString.Exclude private final int METERS_IN_KILOMETER = 1000;
-    @ToString.Exclude private final double HEFFING_SPITS = 1.5;
-    @ToString.Exclude private final double HEFFING_NIET_SPITS = 0.8;
-    @ToString.Exclude private final double PRICE_PER_KM = 0.06;
-    @ToString.Exclude private final double[] MULTIPLICATION_FACTOR_ROADCATEGORIES = {0.5, 0.75, 1, 1.25, 1.5};
     private String driverAssetId;
     private String licensePlate;
     private String brand;
@@ -25,7 +19,6 @@ public class DriverAsset {
     private double[] drivenKilometersOnRoads = new double[5];
 
     private double rideCosts = 0.0;
-    @ToString.Exclude HashMap<String, Double> calcMap;
 
     public DriverAsset() {
     }
@@ -36,20 +29,7 @@ public class DriverAsset {
         this.brand = brand;
         this.emissionType = emissionType;
 
-        calcMap = new HashMap<>(); //This map contains all the different emissiontypes that the RDW has.
-        calcMap.put("G", 1.2);
-        calcMap.put("R", 0.6);
-        calcMap.put("P", 0.8);
-        calcMap.put("T", 1.4);
-        calcMap.put("W", 2.0);
-        calcMap.put("1", 1.6);
-        calcMap.put("2", 1.4);
-        calcMap.put("3", 1.3);
-        calcMap.put("4", 1.2);
-        calcMap.put("5", 1.1);
-        calcMap.put("6", 1.0);
-        calcMap.put("E", 0.8);
-        calcMap.put("Z", 0.6);
+
     }
 
     /**
@@ -70,30 +50,5 @@ public class DriverAsset {
         this.rideCosts += priceOfDrive;
         System.out.println(this.rideCosts);
         System.out.println(Arrays.toString(this.drivenKilometersOnRoads));
-    }
-
-    /**
-     *
-     * @param roadCategory
-     * @param spits
-     * @param distanceTravelled
-     * @return
-     */
-    private double calculateTravelCosts(Integer roadCategory, Integer spits, double distanceTravelled) {
-        /*
-        Divide to kilometers.
-        Times road category
-        Times value for emissie_code. Emissie=emission.
-        Check it is for spits or not.
-         */
-        double price = distanceTravelled / METERS_IN_KILOMETER * PRICE_PER_KM
-                * MULTIPLICATION_FACTOR_ROADCATEGORIES[roadCategory - 1] * calcMap.get(this.emissionType);
-
-        if(spits == 0) {
-            price *= HEFFING_NIET_SPITS;
-        } else {
-            price *= HEFFING_SPITS;
-        }
-        return price;
     }
 }
---
+++ ppk_blockchain/src/main/java/com/capgemini/ppk_blockchain/blockchain/util/CalculateTravelCosts.java
@@ -0,0 +1,59 @@
+package com.capgemini.ppk_blockchain.blockchain.util;
+
+import lombok.ToString;
+
+import java.util.HashMap;
+
+public class CalculateTravelCosts {
+
+    private final int METERS_IN_KILOMETER = 1000;
+    private final double HEFFING_SPITS = 1.5;
+    private final double HEFFING_NIET_SPITS = 0.8;
+    private final double PRICE_PER_KM = 0.06;
+    private final double[] MULTIPLICATION_FACTOR_ROADCATEGORIES = {0.5, 0.75, 1, 1.25, 1.5};
+    HashMap<String, Double> calcMap;
+
+    public CalculateTravelCosts() {
+
+        calcMap = new HashMap<>(); //This map contains all the different emissiontypes that the RDW has.
+        calcMap.put("G", 1.2);
+        calcMap.put("R", 0.6);
+        calcMap.put("P", 0.8);
+        calcMap.put("T", 1.4);
+        calcMap.put("W", 2.0);
+        calcMap.put("1", 1.6);
+        calcMap.put("2", 1.4);
+        calcMap.put("3", 1.3);
+        calcMap.put("4", 1.2);
+        calcMap.put("5", 1.1);
+        calcMap.put("6", 1.0);
+        calcMap.put("E", 0.8);
+        calcMap.put("Z", 0.6);
+    }
+
+    /**
+     *
+     * @param roadCategory
+     * @param spits
+     * @param distanceTravelled
+     * @return
+     */
+    private double calculateTravelCosts(Integer roadCategory, Integer spits, double distanceTravelled, String emissionType) {
+        /*
+        Divide to kilometers.
+        Times road category
+        Times value for emissie_code. Emissie=emission.
+        Check it is for spits or not.
+         */
+        double price = distanceTravelled / METERS_IN_KILOMETER * PRICE_PER_KM
+                * MULTIPLICATION_FACTOR_ROADCATEGORIES[roadCategory - 1] * calcMap.get(this.emissionType);
+
+        if(spits == 0) {
+            price *= HEFFING_NIET_SPITS;
+        } else {
+            price *= HEFFING_SPITS;
+        }
+        return price;
+    }
+
+}
